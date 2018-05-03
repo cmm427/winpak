64b65ee9140ab0ba0158cdf08dfd08bb7d182109
cmm427 <cmm4278@gmail.com>
2018-05-03 22:49:10 +0800

    update validateSign

--- src/main/java/com/jushi/winpak/configurer/WebMvcConfigurer.java
+++ src/main/java/com/jushi/winpak/configurer/WebMvcConfigurer.java
@@ -113,7 +113,7 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
                 @Override
                 public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
                     //验证签名
-                    boolean pass = validateSign1(request);
+                    boolean pass = validateSign(request);
                     if (pass) {
                         return true;
                     } else {
@@ -141,38 +141,11 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
         }
     }
 
-    /**
-     * 一个简单的签名认证，规则：
-     * 1. 将请求参数按ascii码排序
-     * 2. 拼接为a=value&b=value...这样的字符串（不包含sign）
-     * 3. 混合密钥（secret）进行md5获得签名，与请求的签名进行比较
-     */
-    private boolean validateSign(HttpServletRequest request) {
-        String requestSign = request.getParameter("sign");//获得请求签名，如sign=19e907700db7ad91318424a97c54ed57
-        if (StringUtils.isEmpty(requestSign)) {
-            return false;
-        }
-        List<String> keys = new ArrayList<String>(request.getParameterMap().keySet());
-        keys.remove("sign");//排除sign参数
-        Collections.sort(keys);//排序
-
-        StringBuilder sb = new StringBuilder();
-        for (String key : keys) {
-            sb.append(key).append("=").append(request.getParameter(key)).append("&");//拼接字符串
-        }
-        String linkString = sb.toString();
-        linkString = StringUtils.substring(linkString, 0, linkString.length() - 1);//去除最后一个'&'
-
-        String secret = "Potato";//密钥，自己修改
-        String sign = DigestUtils.md5Hex(linkString + secret);//混合密钥md5
-
-        return StringUtils.equals(sign, requestSign);//比较
-    }
 
     /**
      *
      */
-    private boolean validateSign1(HttpServletRequest request) {
+    private boolean validateSign(HttpServletRequest request) {
         String requestSign = request.getParameter("sign");//获得请求签名，如sign=19e907700db7ad91318424a97c54ed57
         if (StringUtils.isEmpty(requestSign)) {
             return false;
